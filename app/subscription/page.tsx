2b6e0621ae17c2e17fb7f344fdc03bd5963263bd
¡Duque <atanaelleonardo@gmail.com>
2024-11-14 01:43:56 -0300

    feat: create subscription page component

---
+++ app/subscription/page.tsx
@@ -0,0 +1,12 @@
+export type SubscriptionPageProps = {
+  title?: string
+}
+
+export const SubscriptionPage = ({ title }: SubscriptionPageProps) => {
+  return (
+    <>
+      <h1>Oi</h1>
+      <p>{title}</p>
+    </>
+  )
+}
