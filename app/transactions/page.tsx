193f6c69337d158e06e55906be70a971d82acbb3
¡Duque <atanaelleonardo@gmail.com>
2024-11-14 01:44:15 -0300

    feat: create transactions page component

---
+++ app/transactions/page.tsx
@@ -0,0 +1,12 @@
+export type TransactionsPageProps = {
+  title?: string
+}
+
+export const TransactionsPage = ({ title }: TransactionsPageProps) => {
+  return (
+    <>
+      <h1>Oi</h1>
+      <p>{title}</p>
+    </>
+  )
+}
